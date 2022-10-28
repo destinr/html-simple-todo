e7690ae04b214f7a780d7cc4696b00b75200409c
destinr <destinroyer@gmail.com>
2022-10-28 13:40:12 -0700

    Meets spec minus crossoff.

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log("Hello World")
+
+let numTasks = 0;
+
+const addTask = () => {
+    numTasks++;
+    let newTask = document.getElementById("newTask").value;
+    const tasksDiv = document.getElementById("TODO items");
+    let taskDiv = document.createElement('div');
+    taskDiv.setAttribute('class', 'todo');
+    taskDiv.setAttribute('id','task'+numTasks)
+    taskDiv.innerHTML = numTasks.toString() + ": " + newTask;
+    tasksDiv.append(taskDiv);
+    taskDiv.setAttribute('onclick', `crossOff(${taskDiv.id})`)
+
+
+    document.getElementById("newTask").value = "";
+}
+
+const crossOff = (element) => {
+    eleText = element.innerHTML
+    if (element.class === 'todo'){
+        console.log(element.class)
+        element.class = "completed"
+    }
+    else {
+        console.log(element.class)
+        element.class = "todo"
+    }
+}
+
+const inputBox = document.getElementById("newTask")
+inputBox.addEventListener("keypress",function(event) {
+    if (event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("submitTask").click();
+    }
+    
+});
